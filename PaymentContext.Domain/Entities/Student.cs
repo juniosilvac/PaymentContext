8fd4cb599971ca000cb187d13b39ba2486739f1e
Junio Silva <junio.silvac@gmail.com>
2018-07-19 21:26:13 -0300

    Criado testes para Student

--- PaymentContext.Domain/Entities/Student.cs
+++ PaymentContext.Domain/Entities/Student.cs
@@ -34,7 +34,8 @@ namespace PaymentContext.Domain.Entities
 
       AddNotifications(new Contract()
                       .Requires()
-                      .IsFalse(hasSubscriptionActive, "Student.Subscriptions", "Você já tem uma assinatura ativa"));
+                      .IsFalse(hasSubscriptionActive, "Student.Subscriptions", "Você já tem uma assinatura ativa")
+                      .AreEquals(0,subscription.Payments.Count,"Student.Subscriptions.Payments","Esta assinatura não possui pagamentos."));
 
       // Alternativa de notificação
       // if(hasSubscriptionActive)
--- PaymentContext.Tests/Entities/StudentTests.cs
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -1,5 +1,7 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
 using PaymentContext.Domain.ValueObjects;
 
 namespace PaymentContext.Tests
@@ -7,9 +9,53 @@ namespace PaymentContext.Tests
   [TestClass]
   public class StudentTests
   {
-    //[TestMethod]
-    public void AdicionarAssinatura()
+    private readonly Name _name;
+    private readonly Document _document;
+    private readonly Address _address;
+    private readonly Email _email;
+    private readonly Student _student;
+    private readonly Subscription _subscription;
+
+    public StudentTests()
     {
+      _name = new Name("Bruce", "Wayne");
+      _document = new Document("56759217020", EDocumentType.CPF);
+      _email = new Email("contato@v.com.br");
+      _address = new Address("Rua Doze", "12", "Bairro Centro","Betim","MG","BR","21343412");
+      _student = new Student(_name, _document, _email);
+      _subscription = new Subscription(null);     
     }
+
+
+    [TestMethod]
+    public void ShoudReturnErrorWhenHadActiveSubscriptin()
+    {
+       var payment = new PayPalPayment("123456",DateTime.Now, DateTime.Now.AddDays(5),10,10,"Way Corp",_document, _address, _email);
+      _subscription.AddPayment(payment);
+
+      _student.AddSubscription( _subscription );
+      _student.AddSubscription( _subscription );
+
+      Assert.IsTrue(_student.Invalid);
+    }
+
+    [TestMethod]
+    public void ShoudReturnErrorWhenHadActiveSubscriptinHasNotPayment()
+    {     
+      _student.AddSubscription( _subscription );
+
+      Assert.IsTrue(_student.Invalid);     
+    }   
+
+    [TestMethod]
+    public void ShoudReturnSuccessWhenAddSubscription()
+    {
+       var payment = new PayPalPayment("123456",DateTime.Now, DateTime.Now.AddDays(5),10,10,"Way Corp",_document, _address, _email);
+      _subscription.AddPayment(payment);
+
+      _student.AddSubscription( _subscription );      
+
+      Assert.IsTrue(_student.Valid);
+    } 
   }
 }
